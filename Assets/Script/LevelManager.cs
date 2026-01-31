a415b5794b14d7b7a0b6fb698c9b43ce5f7a2739
Timothée ANTONIOL <timodelavega@gmail.com>
2026-01-31 15:27:11 +0100

    Card Inventory

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour
+{
+    private List<Card> _playerInventory;
+    private List<Card> _levelStartingInventory;
+
+}
