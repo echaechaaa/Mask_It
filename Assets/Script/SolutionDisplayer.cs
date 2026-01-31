8cf809cfabbd802e413286344eb6f9a722fa4c97
jeremyferreira <jeremyblazerpro@gmail.com>
2026-01-31 19:28:09 +0100

    Soluce

---
+++ Assets/Script/SolutionDisplayer.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class SolutionDisplayer : MonoBehaviour
+{
+    public float CardSize;
+    public List<CardUI> DisplayedCards;
+    public List<CardUI> MaskedCards;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach (CardUI card in DisplayedCards)
+        {
+            Card cardObj = Instantiate(card.Card, transform);
+            cardObj.transform.localPosition = Vector3.zero;
+            cardObj.transform.localScale = Vector3.one * CardSize;
+
+            cardObj.Showcard();
+        }
+
+        foreach (CardUI card in MaskedCards)
+        {
+            Card cardObj = Instantiate(card.Card, transform);
+            cardObj.transform.localPosition = Vector3.zero;
+            cardObj.transform.localScale = Vector3.one * CardSize;
+
+            cardObj.MaskCard();
+        }
+    }
+
+    
+}
